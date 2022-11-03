a2a0a12e4311d1b5b4b8b0036049f67644597474
ShannaSmith <shannagjh@gmail.com>
2022-11-02 22:37:57 -0400

    add function

--- produce_summary.py
+++ produce_summary.py
@@ -1,3 +1,18 @@
+def create_delivery_report(day, um_deliveries):
+    day = input("Enter deliver day")
+    print(day)
+    the_file = open(um_deliveries)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
 print("Day 1")
 the_file = open("um-deliveries-day-1.txt")
 for line in the_file:
@@ -5,8 +20,8 @@ for line in the_file:
     words = line.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
