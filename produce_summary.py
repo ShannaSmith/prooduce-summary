c78c27b59959d62c3b8b26c29998e0137486a30e
ShannaSmith <shannagjh@gmail.com>
2022-11-02 23:04:28 -0400

    function works

--- produce_summary.py
+++ produce_summary.py
@@ -1,5 +1,4 @@
 def create_delivery_report(day, um_deliveries):
-    day = input("Enter deliver day")
     print(day)
     the_file = open(um_deliveries)
     for line in the_file:
@@ -11,45 +10,4 @@ def create_delivery_report(day, um_deliveries):
         amount = words[2]
         print(f"Delivered {count} {melon}s for total of ${amount}")
     the_file.close()
-
-
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+create_delivery_report("Day 1", "um-deliveries-day-1.txt" )
